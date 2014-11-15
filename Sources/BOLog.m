4f130a4982e57150c3af1ff0e84bb1a1a5079919
Kevin Wojniak <kainjow@kainjow.com>
2014-11-15 08:22:54 -0800

    Remove old logs.

--- Sources/BOLog.m
+++ Sources/BOLog.m
@@ -21,12 +21,34 @@
     return sharedLog;
 }
 
+- (void)removeOldLogs:(NSString *)logDir
+{
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSMutableArray *logFiles = [NSMutableArray array];
+    for (NSString *filename in [fm contentsOfDirectoryAtPath:logDir error:nil]) {
+        if ([filename.pathExtension isEqualToString:@"txt"]) {
+            NSString *fullpath = [logDir stringByAppendingPathComponent:filename];
+            NSDate *fileDate = [fm attributesOfItemAtPath:fullpath error:nil].fileCreationDate;
+            NSDictionary *dict = @{@"date" : fileDate, @"path" : fullpath};
+            [logFiles addObject:dict];
+        }
+    }
+    [logFiles sortUsingDescriptors:@[[[NSSortDescriptor alloc] initWithKey:@"date" ascending:YES]]];
+    const NSUInteger maxLogs = 5;
+    while (logFiles.count > (maxLogs - 1)) {
+        NSDictionary *dict = [logFiles objectAtIndex:0];
+        [logFiles removeObjectAtIndex:0];
+        (void)[fm removeItemAtPath:dict[@"path"] error:nil];
+    }
+}
+
 - (id)init
 {
     if ((self = [super init]) != nil) {
         NSString *logDir = [@"~/Library/Application Support/BootChamp/Logs" stringByStandardizingPath];
         NSFileManager *fm = [NSFileManager defaultManager];
         (void)[fm createDirectoryAtPath:logDir withIntermediateDirectories:YES attributes:nil error:nil];
+        [self removeOldLogs:logDir];
         NSString *logFileName = [NSString stringWithFormat:@"%@.txt", [[NSDate date] descriptionWithCalendarFormat:@"%Y%m%d%H%M%S" timeZone:[NSTimeZone timeZoneForSecondsFromGMT:0] locale:nil]];
         NSString *logPath = [logDir stringByAppendingPathComponent:logFileName];
         (void)[fm createFileAtPath:logPath contents:nil attributes:nil];
